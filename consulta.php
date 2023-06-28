d96c50fa7ec88b4a11c3ea3386fdd4f90a14bf87
JoaoZenaro <47560029+JoaoZenaro@users.noreply.github.com>
2023-06-28 17:07:40 +0000

    roteiro p. 24

---
+++ consulta.php
@@ -0,0 +1,101 @@
+<?php
+require_once "conexoes.php";
+require_once 'utils.php';
+$conn = conectarPDO();
+$nome_pesquisa = $_GET['nome_pesquisa'] ?? '';
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Listagem com Filtro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container" id="listagem_alunos">
+        <form action="consulta.php" method="get">
+            <div class="d-flex mt-2 p-3 bg-secondary">
+                <div class="input-group col-10 busca">
+                    <span class="input-group-text">
+                        <i class="fa fa-search"></i>
+                    </span>
+                    <div class="form-floating">
+                        <input id="filtro" type="search" name="nome_pesquisa" class="form-control" value="<?=$nome_pesquisa ?>" placeholder="Entre com o nome do aluno">
+                        <label for="filtro" class="pt-2">Entre com o nome do aluno</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Buscar</button>
+                    <div class="col-1"></div>
+                    <button id="btnLimpar" type="button" class="btn btn-danger">Limpar</button>
+                </div>
+            </div>
+        </form>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+                <caption>Relação de Alunos</caption>
+                <thead class="table-dark">
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Nascimento</th>
+                        <th>Salário (R$)</th>
+                        <th>Foto</th>
+                    </tr>
+                </thead>
+                <?php
+                $filtro = "%{$nome_pesquisa}%";
+                $stmt = $conn->prepare('SELECT * FROM aluno WHERE nome LIKE :nome_aluno');
+                $stmt->bindParam(':nome_aluno', $filtro, PDO::PARAM_STR);
+                $stmt->execute();
+                while ($aluno = $stmt->fetch()) {
+                    $data_nascimento = date('d-m-Y', strtotime($aluno['nascimento']));
+                    $salario = number_format($aluno['salario'], 2, ',', '.');
+                ?>
+                    <tr>
+                        <td style="width: 10%;"><?php echo $aluno['id_aluno'] ?></td>
+                        <td style="width: 30%;">
+                            <a href="detalhes.php?id_aluno=<?php echo $aluno['id_aluno'] ?>">
+                                <?=$aluno['nome'] ?>
+                            </a>
+                        </td>
+                        <td style="width: 20%;" class="text-center"><?=$data_nascimento ?></td>
+                        <td style="width: 15%;" class="text-end"><?=$salario ?></td>
+                        <td style="width: 25%;" class="imagem">
+                            <a href="detalhes.php?id_aluno=<?=$aluno['id_aluno'] ?>">
+                                <?php echo '<img src="data:image/png;base64,' . ($aluno['foto'] ? base64_encode($aluno['foto']) : '') . '" width="200px"/>'; ?>
+                                <!-- <?php echo '<img src="data:image/png;base64,' . base64_encode($aluno['foto']) . '" width="200px"/>'; ?> -->
+                            </a>
+                        </td>
+                    </tr>
+                <?php
+                }
+                $stmt = null;
+                $conn = null;
+                ?>
+                <tfoot>
+                    <tr>
+                        <td colspan="5" style="text-align: center">
+                            Data atual: <?=retornarDataAtual() ?>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $("#btnLimpar").on("click", function (e) {
+                e.preventDefault();
+                $("#filtro").val("");
+                window.location = "consulta.php";
+            });
+        });
+    </script>
+</body>
+</html>
--- listaPDO.php
+++ listaPDO.php
@@ -18,6 +18,13 @@ function listarDadosPDO($filtro = '%%')
 <th>Salário (R$)</th>
  </tr>
  </thead>';
+
+// echo '<code><pre>';
+// var_dump($stmt->fetch());
+// echo '<br>';
+// print_r($stmt->fetch());
+// echo '</pre></code>';
+
     while ($aluno = $stmt->fetch()) {
         $data_nascimento = date('d-m-Y', strtotime($aluno['nascimento']));
         $salario = number_format($aluno['salario'], 2, ',', '.');
@@ -31,6 +38,57 @@ function listarDadosPDO($filtro = '%%')
     echo '<tfoot><tr><td colspan="5" style="text-align: center">Data atual: ' . retornarDataAtual() .
         '</td></tr>';
     echo '</table></div>';
+
+
+    
+    $stmt->execute();
+    $alunos_num = $stmt->fetchAll(PDO::FETCH_NUM);
+    
+    $stmt->execute();
+    $alunos_assoc = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    $stmt->execute();
+    $alunos_obj = $stmt->fetchAll(PDO::FETCH_OBJ);
+    
+    echo '<br><h4>Lista de Alunos (<code>PDO::FETCH_NUM</code> / <code>foreach()</code>)</h4>';
+    echo '<ul>';
+    
+    foreach ($alunos_num as $aluno) {
+        echo "<li>ID: <strong>{$aluno[0]}</strong> - ";
+        echo "Nome: <strong>{$aluno[1]}</strong></li>";
+    }
+    
+    echo '</ul>';
+    echo '<br><h4>Lista de Alunos (<code>PDO::FETCH_ASSOC</code> / <code>foreach()</code> associativo)</h4>';
+    echo '<ul>';
+
+    foreach ($alunos_assoc as $aluno) {
+        echo '<li>';
+        foreach ($aluno as $campo => $valor) {
+            echo "$campo: <strong>$valor</strong> - ";
+        }
+        echo '</li>';
+    }
+    echo '</ul>';
+    echo '<br><h4>Lista de Alunos (<code>PDO::FETCH_NUM</code> / <code>for()</code> com matriz)</h4>';
+    echo '<ul>';
+    
+    for ($i = 0; $i < $stmt->rowCount(); $i++) {
+        echo "<li>ID: <strong>{$alunos_num[$i][0]}</strong> - ";
+        echo "Nome: <strong>{$alunos_num[$i][1]}</strong></li>";
+    }
+
+    echo '</ul>';
+    echo '<br><h4>Lista de Alunos (<code>PDO::FETCH_OBJ</code> / <code>foreach()</code>)</h4>';
+    echo '<ul>';
+
+    foreach ($alunos_obj as $aluno) {
+        echo "<li>ID: <strong>{$aluno->id_aluno}</strong> - ";
+        echo "Nome: <strong>{$aluno->nome}</strong></li>";
+    }
+    echo '</ul>';
+
+
     // Fecha consulta e conexão, liberando recursos
     $stmt = null;
     $conn = null;
